aad07f90c5fe613a1b2b215235b800e0e33db74b
Amina <amina_elkharraz@hotmail.com>
2023-03-21 18:39:55 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
  */
 void print_to_98(int n)
 {
-	int i, i;
+	int i, j;
 
 	if (n <= 98)
 	{
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			printf("%d\n", i);
 	} else if (n >= 98)
 	{
-		for (j = n. j >= 98; j--)
+		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
 				printf("%d, ", j);
