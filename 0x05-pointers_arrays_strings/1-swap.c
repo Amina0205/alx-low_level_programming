a9b49a59d5b1e1d506a3e5b12efdc2a465b4abb5
Amina <amina_elkharraz@hotmail.com>
2023-03-27 11:24:35 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * swap_int - swaps the value of int a and int b
  * Owned By Bwave/Bright
- * @a: first int 
+ * @a: first int
  * @b: second int
- * Return: 0
+ * Return: 0 (Success)
  */
+
 void swap_int(int *a, int *b)
 {
 	int s;
